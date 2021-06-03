22b35ebbd53266e3f52e46b28ffd45bc2e216052
theDanielTo <dto1989@gmail.com>
2021-06-03 14:40:33 -0700

    removed car object, event listen adds style prop to img tag #2

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 const carPerspective = {
-  right: 'face-right',
-  down: 'face-down',
-  left: 'face-left',
-  up: 'face-up'
+  right: 360,
+  down: 90,
+  left: 180,
+  up: 270
 };
-
-// const car = {
-//   turn: 0,
-//   start: 0,
-//   stop: 0,
-//   steer: 0
-// };
 
 const carEl = document.querySelector('img');
 
 document.addEventListener('keydown', function (event) {
-  const curPerspective = carEl.className;
-  if (event.key === 'ArrowRight') carEl.classList.replace(curPerspective, carPerspective.right);
-  if (event.key === 'ArrowDown') carEl.classList.replace(curPerspective, carPerspective.down);
-  if (event.key === 'ArrowLeft') carEl.classList.replace(curPerspective, carPerspective.left);
-  if (event.key === 'ArrowUp') carEl.classList.replace(curPerspective, carPerspective.up);
+  if (event.key === 'ArrowRight') {
+    carEl.style.transform = 'rotate(' + carPerspective.right + 'deg)';
+  } else if (event.key === 'ArrowDown') {
+    carEl.style.transform = 'rotate(' + carPerspective.down + 'deg)';
+  } else if (event.key === 'ArrowLeft') {
+    carEl.style.transform = 'rotate(' + carPerspective.left + 'deg)';
+  } else {
+    carEl.style.transform = 'rotate(' + carPerspective.up + 'deg)';
+  }
 });
