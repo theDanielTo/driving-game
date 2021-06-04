362a8e2655b619a08181a26ce9927fd527fbcb94
theDanielTo <dto1989@gmail.com>
2021-06-03 17:46:26 -0700

    Completed issue 3

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-const carPerspective = {
+const carData = {
   right: 'face-right',
   down: 'face-down',
   left: 'face-left',
-  up: 'face-up'
+  up: 'face-up',
+  facing: 'right',
+  xPos: 0,
+  yPos: 0
 };
-
+let intervalID = null;
 const carEl = document.querySelector('img');
 
 document.addEventListener('keydown', function (event) {
+  clearInterval(intervalID);
   const curPerspective = carEl.className;
-  if (event.key === 'ArrowRight') carEl.classList.replace(curPerspective, carPerspective.right);
-  if (event.key === 'ArrowDown') carEl.classList.replace(curPerspective, carPerspective.down);
-  if (event.key === 'ArrowLeft') carEl.classList.replace(curPerspective, carPerspective.left);
-  if (event.key === 'ArrowUp') carEl.classList.replace(curPerspective, carPerspective.up);
+  if (event.key === 'ArrowRight') {
+    carEl.classList.replace(curPerspective, carData.right);
+    carData.facing = 'right';
+  } else if (event.key === 'ArrowDown') {
+    carEl.classList.replace(curPerspective, carData.down);
+    carData.facing = 'down';
+  } else if (event.key === 'ArrowLeft') {
+    carEl.classList.replace(curPerspective, carData.left);
+    carData.facing = 'left';
+  } else if (event.key === 'ArrowUp') {
+    carEl.classList.replace(curPerspective, carData.up);
+    carData.facing = 'up';
+  } else if (event.key === ' ') {
+    startCar(carData.facing);
+  }
 });
+
+function startCar(direction) {
+  const styleDirection = (direction === 'left' || direction === 'right') ? 'left' : 'top';
+  const position = (direction === 'right' || direction === 'left') ? 'xPos' : 'yPos';
+  const pxMove = (direction === 'left' || direction === 'up') ? -4 : 4;
+
+  intervalID = setInterval(function () {
+    carData[position] += pxMove;
+    carEl.style[styleDirection] = carData[position].toString() + 'px';
+  }, 16);
+}
